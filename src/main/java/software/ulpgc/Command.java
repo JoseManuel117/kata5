c1f1920f27678e93672a0068363edc2cd74369b8
JoseManuel117 <jmksd117@gmail.com>
2024-01-20 10:24:55 +0000

    commit final

---
+++ src/main/java/software/ulpgc/Command.java
@@ -0,0 +1,27 @@
+package software.ulpgc;
+
+public interface Command {
+    Output execute(Input input);
+    interface Input{
+        String get(String key);
+    }
+
+    interface Output{
+        int code();
+        String result();
+    }
+
+    static Output output(int code, String result){
+        return new Output() {
+            @Override
+            public int code() {
+                return code;
+            }
+
+            @Override
+            public String result() {
+                return result;
+            }
+        };
+    }
+}
---
+++ src/main/java/software/ulpgc/Main.java
@@ -0,0 +1,36 @@
+package software.ulpgc;
+
+import spark.Request;
+import spark.Response;
+import spark.Spark;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Spark.port(8080);
+        Spark.get("/factorial", (req,res)->new CommandExecutor(req,res).execute(new FactorialCommand()));
+    }
+
+    private record CommandExecutor(Request req, Response res) {
+        public Object execute(Command command) {
+            Command.Output output = command.execute(input());
+            res.status(output.code());
+            return output.result();
+        }
+
+        private Command.Input input() {
+            return new Command.Input() {
+                @Override
+                public String get(String key) {
+                    return oneOf(req.params(key), req.queryParams(key));
+                }
+
+                private String oneOf(String a, String b) {
+                    return a != null ? a : b;
+                }
+            };
+        }
+    }
+}
